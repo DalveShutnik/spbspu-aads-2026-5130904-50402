9517e9ed19f1e90a7fe94d97c3ff113dfb516347
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-05-31 19:17:42 +0300

    add stack

--- samarin.denis/S2/stack.hpp
+++ samarin.denis/S2/stack.hpp
@@ -1,6 +1,7 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+#include <stdexcept>
 #include "list.hpp"
 
 namespace samarin {
@@ -8,9 +9,35 @@ namespace samarin {
   template< class T >
   class Stack {
   public:
-    void push(const T& rhs);
-    T drop();
-    bool empty() const;
+    bool empty() const
+    {
+      return data_.empty();
+    }
+
+    T& top()
+    {
+      return data_.front();
+    }
+
+    const T& top() const
+    {
+      return data_.front();
+    }
+
+    void push(const T& rhs)
+    {
+      data_.push_front(rhs);
+    }
+
+    T drop()
+    {
+      if (data_.empty()) {
+        throw std::logic_error("stack is empty");
+      }
+      T value = data_.front();
+      data_.pop_front();
+      return value;
+    }
 
   private:
     List< T > data_;
