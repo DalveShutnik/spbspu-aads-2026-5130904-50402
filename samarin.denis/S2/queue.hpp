899b4fa50393f5c7999d6bfed757777bf361f7a1
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-05-31 19:18:04 +0300

    add queue

--- samarin.denis/S2/queue.hpp
+++ samarin.denis/S2/queue.hpp
@@ -1,6 +1,7 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+#include <stdexcept>
 #include "list.hpp"
 
 namespace samarin {
@@ -8,12 +9,75 @@ namespace samarin {
   template< class T >
   class Queue {
   public:
-    void push(const T& rhs);
-    T drop();
-    bool empty() const;
+    Queue():
+      data_(),
+      tail_(data_.before_begin())
+    {}
+
+    Queue(const Queue< T >& other):
+      data_(other.data_),
+      tail_(data_.before_begin())
+    {
+      for (LIter< T > it = data_.begin(); it != data_.end(); ++it) {
+        tail_ = it;
+      }
+    }
+
+    Queue(Queue< T >&& other) noexcept:
+      data_(std::move(other.data_)),
+      tail_(other.tail_)
+    {}
+
+    Queue< T >& operator=(const Queue< T >& other)
+    {
+      if (this != std::addressof(other)) {
+        Queue< T > tmp(other);
+        std::swap(data_, tmp.data_);
+        std::swap(tail_, tmp.tail_);
+      }
+      return *this;
+    }
+
+    Queue< T >& operator=(Queue< T >&& other) noexcept
+    {
+      if (this != std::addressof(other)) {
+        data_ = std::move(other.data_);
+        tail_ = other.tail_;
+      }
+      return *this;
+    }
+
+    bool empty() const
+    {
+      return data_.empty();
+    }
+
+    void push(const T& rhs)
+    {
+      if (data_.empty()) {
+        data_.push_front(rhs);
+        tail_ = data_.begin();
+      } else {
+        tail_ = data_.insert_after(tail_, rhs);
+      }
+    }
+
+    T drop()
+    {
+      if (data_.empty()) {
+        throw std::logic_error("queue is empty");
+      }
+      T value = data_.front();
+      data_.pop_front();
+      if (data_.empty()) {
+        tail_ = data_.before_begin();
+      }
+      return value;
+    }
 
   private:
     List< T > data_;
+    LIter< T > tail_;
   };
 
 }
