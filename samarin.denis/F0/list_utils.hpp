07ea1192f2058eb26b78510981903f097251c10a
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-03 17:07:31 +0300

    add list merge sort

--- samarin.denis/F0/list_utils.hpp
+++ samarin.denis/F0/list_utils.hpp
@@ -65,6 +65,57 @@ namespace samarin {
   }
 
   template< class T >
+  List< T > mergeSorted(const List< T >& left, const List< T >& right)
+  {
+    List< T > out;
+    LIter< T > tail = out.before_begin();
+    LCIter< T > a = left.cbegin();
+    LCIter< T > b = right.cbegin();
+    while (a != left.cend() && b != right.cend()) {
+      if (*b < *a) {
+        tail = out.insert_after(tail, *b);
+        ++b;
+      } else {
+        tail = out.insert_after(tail, *a);
+        ++a;
+      }
+    }
+    while (a != left.cend()) {
+      tail = out.insert_after(tail, *a);
+      ++a;
+    }
+    while (b != right.cend()) {
+      tail = out.insert_after(tail, *b);
+      ++b;
+    }
+    return out;
+  }
+
+  template< class T >
+  void listSort(List< T >& list)
+  {
+    const std::size_t count = listSize(list);
+    if (count < 2) {
+      return;
+    }
+    List< T > left;
+    List< T > right;
+    LIter< T > leftTail = left.before_begin();
+    LIter< T > rightTail = right.before_begin();
+    std::size_t index = 0;
+    for (LCIter< T > it = list.cbegin(); it != list.cend(); ++it, ++index) {
+      if (index < count / 2) {
+        leftTail = left.insert_after(leftTail, *it);
+      } else {
+        rightTail = right.insert_after(rightTail, *it);
+      }
+    }
+    listSort(left);
+    listSort(right);
+    list = mergeSorted(left, right);
+  }
+
+  template< class T >
   void sortedInsert(List< T >& list, const T& value)
   {
     LIter< T > prev = list.before_begin();
