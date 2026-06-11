8c3e3533eb919051dee6a112cecd47932c9f5a31
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-11 15:48:57 +0300

    wire main

--- samarin.denis/S3/main.cpp
+++ samarin.denis/S3/main.cpp
@@ -1,5 +1,8 @@
 #include <fstream>
 #include <iostream>
+#include "commands.hpp"
+#include "graph_collection.hpp"
+#include "input.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -11,6 +14,14 @@ int main(int argc, char* argv[])
   if (!file.is_open()) {
     std::cerr << "cannot open input file\n";
     return 1;
+  }
+  samarin::GraphCollection graphs;
+  try {
+    samarin::readGraphs(file, graphs);
+    samarin::processCommands(std::cin, std::cout, graphs);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << "\n";
+    return 2;
   }
   return 0;
 }
