49e1727536d72799c81ced1858672190bb613ca2
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-17 15:30:00 +0300

    validate bind arguments

--- samarin.denis/S3/commands.cpp
+++ samarin.denis/S3/commands.cpp
@@ -123,6 +123,9 @@ namespace samarin {
     std::string to;
     Graph::Weight weight = 0;
     in >> name >> from >> to >> weight;
+    if (!in) {
+      throw std::logic_error("invalid arguments");
+    }
     requireGraph(graphs, name).bind(from, to, weight);
   }
 
