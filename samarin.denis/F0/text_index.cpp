1e2ecd7cd760748b4cf7a667aa5c4f7f1a980242
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-28 10:06:46 +0300

    use Position in text_index source

--- samarin.denis/F0/text_index.cpp
+++ samarin.denis/F0/text_index.cpp
@@ -11,14 +11,14 @@ namespace samarin {
 
   namespace {
     struct PositionHash {
-      std::size_t operator()(const position_t& pos) const
+      std::size_t operator()(const Position& pos) const
       {
         const std::size_t spread = 1000003ull;
         return detail::cuckooMix(pos.line * spread + pos.word);
       }
     };
 
-    using ForwardTable = CuckooTable< position_t, std::string, PositionHash >;
+    using ForwardTable = CuckooTable< Position, std::string, PositionHash >;
   }
 
 }
@@ -32,7 +32,7 @@ void samarin::TextIndex::build(const Text& text)
   for (LCIter< Line > line = text.cbegin(); line != text.cend(); ++line, ++lineNo) {
     std::size_t col = 0;
     for (LCIter< std::string > word = line->cbegin(); word != line->cend(); ++word, ++col) {
-      const position_t pos = { lineNo, col };
+      const Position pos = { lineNo, col };
       freshPostings[*word].push_front(pos);
     }
     lenTail = freshLengths.insert_after(lenTail, col);
@@ -54,7 +54,7 @@ samarin::Text samarin::TextIndex::restore() const
 {
   ForwardTable forward(wordCount() * 2 + 1);
   for (PostingsTable::const_iterator entry = postings_.cbegin(); entry != postings_.cend(); ++entry) {
-    for (LCIter< position_t > pos = entry->second.cbegin(); pos != entry->second.cend(); ++pos) {
+    for (LCIter< Position > pos = entry->second.cbegin(); pos != entry->second.cend(); ++pos) {
       forward.add(*pos, entry->first);
     }
   }
@@ -65,7 +65,7 @@ samarin::Text samarin::TextIndex::restore() const
     Line words;
     LIter< std::string > wordTail = words.before_begin();
     for (std::size_t col = 0; col < *len; ++col) {
-      const position_t pos = { lineNo, col };
+      const Position pos = { lineNo, col };
       wordTail = words.insert_after(wordTail, forward.at(pos));
     }
     lineTail = text.insert_after(lineTail, words);
