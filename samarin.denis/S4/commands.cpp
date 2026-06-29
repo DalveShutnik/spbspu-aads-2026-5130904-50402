c5ddf89bc1cfc4a61ede71e64f6eb6a17a015e8b
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-29 15:27:12 +0300

    add print command

--- samarin.denis/S4/commands.cpp
+++ samarin.denis/S4/commands.cpp
@@ -13,9 +13,37 @@ namespace samarin {
   using CommandHandler = void (*)(std::istream &, std::ostream &, DatasetCollection &);
   using CommandTable = BSTree< std::string, CommandHandler >;
 
+  static Dataset & requireDataset(DatasetCollection & datasets, const std::string & name)
+  {
+    DatasetCollection::iterator it = datasets.find(name);
+    if (it == datasets.end()) {
+      throw std::logic_error("no such dataset");
+    }
+    return it->second;
+  }
+
+  static void cmdPrint(std::istream & in, std::ostream & out, DatasetCollection & datasets)
+  {
+    std::string name;
+    if (!(in >> name)) {
+      throw std::logic_error("missing name");
+    }
+    const Dataset & dataset = requireDataset(datasets, name);
+    if (dataset.empty()) {
+      out << "<EMPTY>\n";
+      return;
+    }
+    out << name;
+    for (Dataset::const_iterator it = dataset.cbegin(); it != dataset.cend(); ++it) {
+      out << " " << it->first << " " << it->second;
+    }
+    out << "\n";
+  }
+
   static CommandTable makeCommands()
   {
     CommandTable commands;
+    commands.push("print", &cmdPrint);
     return commands;
   }
 
